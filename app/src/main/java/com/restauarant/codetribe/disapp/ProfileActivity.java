874abbcc253a8cabd7f99901f9ce8bea32deec49
JustNsizwa <justismsizwa@gmail.com>
2016-03-22 14:37:18 +0200

    just added few java class

---
+++ app/src/main/java/com/restauarant/codetribe/disapp/ProfileActivity.java
@@ -0,0 +1,19 @@
+package com.restauarant.codetribe.disapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by CodeTribe1 on 3/22/2016.
+ */
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+
+    }
+}
---
+++ app/src/main/java/com/restauarant/codetribe/disapp/Register.java
@@ -0,0 +1,28 @@
+package com.restauarant.codetribe.disapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by CodeTribe1 on 3/22/2016.
+ */
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        Button signUp = (Button)findViewById(R.id.idSignpp);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Register.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
