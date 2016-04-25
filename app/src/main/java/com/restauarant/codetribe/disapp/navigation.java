bdc4d0c20a2660b35c29f0de21ddf72995a91cec
SaviourM <molatudisaviour@gmail.com>
2016-04-25 11:09:02 -0700

    Main Activity, navigation,Register, Splash

--- app/src/main/java/com/restauarant/codetribe/disapp/navigation.java
+++ app/src/main/java/com/restauarant/codetribe/disapp/navigation.java
@@ -11,6 +11,7 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.Menu;
 import android.view.View;
 
 /**
@@ -26,14 +27,6 @@ public class navigation extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
